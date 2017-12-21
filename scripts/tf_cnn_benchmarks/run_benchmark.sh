10cf78dc115a18796361a189c88c6859442dd6e3
Thorsten Kurth <thorstenkurth@me.com>
2017-12-21 07:47:46 -0800

    tf benchmark

--- scripts/tf_cnn_benchmarks/run_benchmark.sh
+++ scripts/tf_cnn_benchmarks/run_benchmark.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-#SBATCH -N 5
+#SBATCH -N 1
 #SBATCH -A nstaff
 #SBATCH -t 00:30:00
 #SBATCH -C knl,quad,cache
@@ -16,8 +15,8 @@ export KMP_SETTINGS=1
 export KMP_AFFINITY="granularity=fine,compact,1,0"
 
 #some other parameters
-num_ps=1
-model='resnet50' #'vgg16'
+num_ps=0
+model='resnet152' #'vgg16'
 
 #run the stuff
-srun -u -n 5 python tf_cnn_benchmarks.py --num_ps=${num_ps} --model=${model} --num_inter_threads=2 --num_intra_threads=32
+srun -u -n 1 python tf_cnn_benchmarks.py --num_ps=${num_ps} --model=${model} --num_inter_threads=2 --num_intra_threads=32 #--data_dir='/global/project/projectdirs/dasrepo/amathuri/Datasets/ImagenetFromMahmoud/imagenet-data'
