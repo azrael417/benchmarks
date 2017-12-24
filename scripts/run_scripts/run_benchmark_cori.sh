f250e12eb20ab0dc4f06b0485e4bcfabe9c6629a
Thorsten Kurth <thorstenkurth@me.com>
2017-12-23 17:12:58 -0800

    adding fixes to distributed Cori script

--- scripts/run_scripts/run_benchmark_cori.sh
+++ scripts/run_scripts/run_benchmark_cori.sh
@@ -15,8 +15,15 @@ export KMP_SETTINGS=1
 export KMP_AFFINITY="granularity=fine,compact,1,0"
 
 #some other parameters
-num_ps=0
-model='resnet50' #'vgg16'
+if [ $SLURM_NNODES -ge 2 ]; then
+    num_ps=1
+else
+    num_ps=0
+fi
+num_workers=$(( ${SLURM_NNODES} - ${num_ps} ))
+
+#model selection
+model='resnet152' #'vgg16'
 
 #run the stuff
-srun -u -n 1 python tf_cnn_benchmarks.py --num_ps=${num_ps} --model=${model} --num_inter_threads=2 --num_intra_threads=32 #--data_dir='/global/project/projectdirs/dasrepo/amathuri/Datasets/ImagenetFromMahmoud/imagenet-data'
+srun -u -n ${SLURM_NNODES} python tf_cnn_benchmarks.py --num_ps=${num_ps} --model=${model} --num_inter_threads=2 --num_intra_threads=32 --host_prefix="nid" --num_host_digits=5 #--data_dir='/global/project/projectdirs/dasrepo/amathuri/Datasets/ImagenetFromMahmoud/imagenet-data'
